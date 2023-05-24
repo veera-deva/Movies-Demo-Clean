bc03aac0ae19f31dd8f5cc0ae14203818e8bbc62
veera <Veerachamy.Devarajan@marks-and-spencer.com>
2023-05-24 17:58:19 +0530

    code refactoring.

--- common-ui/src/main/java/com/demo/common_ui/utils/ProgressDialogUtil.kt
+++ common-ui/src/main/java/com/demo/common_ui/utils/ProgressDialogUtil.kt
@@ -6,11 +6,11 @@ import android.view.Window
 import com.demo.common_ui.R
 
 object ProgressDialogUtil {
-    fun setProgressDialog(context: Context): Dialog {
-        val dialog = Dialog(context)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.progress_indicator)
-        return dialog
-    }
+    fun setProgressDialog(context: Context): Dialog =
+        Dialog(context).apply {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            setCancelable(false)
+            setContentView(R.layout.progress_indicator)
+        }
+
 }
